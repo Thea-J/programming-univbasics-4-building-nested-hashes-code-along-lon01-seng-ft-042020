2fcd846e13e1e4f3cd477048ef609c4d3c815fb1
Dorothy John <dorothyy.john@gmail.com>
2020-04-13 14:38:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague", :age => "53" 
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague", :age => "53" 
         },
       :hero => {
         #your key/value pairs here
